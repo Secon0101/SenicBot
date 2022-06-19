3de476cf251f482c04c6d6f25bff078bdf19fbf3
Secon0101 <duwnsgur11012@gmail.com>
2022-06-19 20:28:40 +0900

    뼈대 추가

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
-    print("Hello, world!")
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print("Logged in")
+
 
 if __name__ == "__main__":
-    main()
+    with open("token.txt", "r") as f:
+        token = f.read()
+        bot.run(token)
